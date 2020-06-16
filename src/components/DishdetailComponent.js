e13080e0d859f76675883724bbc43f7b8fa3f582
Mustafa Abdelrahman <mustafausama@outlook.com>
2020-06-16 17:44:53 +0300

    Functional Components set

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,55 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-        console.log(this.props);
-    }
-    renderDish(dish) {
-        if(dish) return (
+
+function RenderDish({dish}) {
+    if(dish) return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                <RenderComments comments={dish.comments} />
+            </div>
+        </div>
+    );
+    return (
+        <div></div>
+    );
+}
+
+function RenderComments({comments}) {
+    const coms = comments.map((comment) => {
+        return (
             <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    {this.renderComments(dish.comments)}
-                </div>
+                <li id={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author} , {(new Date(comment.date)).toLocaleString('default', { month: 'short', day: 'numeric', year: 'numeric' })}</p>
+                </li>
             </div>
         );
-        return (
-            <div></div>
-        );
-    }
-    renderComments(comments) {
-        const coms = comments.map((comment) => {
-            return (
-                <div className="container">
-                    <li id={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author} , {(new Date(comment.date)).toLocaleString('default', { month: 'short', day: 'numeric', year: 'numeric' })}</p>
-                    </li>
-                </div>
-            );
-        });
-        return (
-            <div className="col-12 col-md-5 m1">
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {coms}
-                </ul>
-            </div>
-        );
-    }
-    render() {
-        return this.renderDish(this.props.dish);
-    }
+    });
+    return (
+        <div className="col-12 col-md-5 m1">
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                {coms}
+            </ul>
+        </div>
+    );
+}
+const DishDetail = (props) => {
+    return <RenderDish dish={props.dish} />;
 }
 
 export default DishDetail;
