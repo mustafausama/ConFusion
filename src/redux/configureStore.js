f9818f24f5051d725bc19c315672c1abd395fa08
Mustafa Abdelrahman <mustafausama@outlook.com>
2020-08-25 18:12:46 +0200

    Redux almost completed, redux form updated

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,25 @@
-import { createStore, combineReducers } from 'redux';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Leaders } from './leaders';
-import { Promotions } from './promotions';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { InitialFeedback } from "./forms";
 
 export const ConfigureStore = () => {
-	const store = createStore(
-		combineReducers({
-			dishes: Dishes,
-			comments: Comments,
-			leaders: Leaders,
-			promotions: Promotions
-		}),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	);
-	return store;
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      leaders: Leaders,
+      promotions: Promotions,
+      ...createForms({ feedback: InitialFeedback }),
+    }),
+    applyMiddleware(thunk, logger)
+  );
+  return store;
 };
+/*
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
